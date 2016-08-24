77fea13caadcd61f8af69d0f85a3398225d471a2
Goran Udosic <udosic.goran@gmail.com>
2016-08-24 23:52:06 +0200

    Support programmatic removal of notifications #6

--- src/notifications.js
+++ src/notifications.js
@@ -13,6 +13,15 @@ class Notifications extends React.Component {
 
   componentWillReceiveProps(nextProps) {
     const {notifications} = nextProps;
+    const notificationIds = notifications.map(notification => notification.uid);
+
+    // Get all active notifications from react-notification-system
+    /// and remove all where uid is not found in the reducer
+    (this.system().state.notifications || []).forEach(notification => {
+      if (notificationIds.indexOf(notification.uid) < 0) {
+        this.system().removeNotification(notification.uid);
+      }
+    });
 
     notifications.forEach(notification => {
       this.system().addNotification({
