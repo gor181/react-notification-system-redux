6b87551af4d5402666d8ce37b854a347fb007a03
Goran Udosic <udosic.goran@gmail.com>
2016-12-03 16:04:07 +0100

    Update 2016-12-03T15:04:05.326Z

--- bundle.js
+++ bundle.js
@@ -105,27 +105,9 @@ function Notifications() {
 }
 
 },{"./const":2}],"react-notification-system-redux":[function(require,module,exports){
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.reducer = undefined;
-
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _reducer = require('./reducer');
-
-Object.defineProperty(exports, 'reducer', {
-  enumerable: true,
-  get: function () {
-    function get() {
-      return _interopRequireDefault(_reducer)['default'];
-    }
-
-    return get;
-  }()
-});
 
 var _react = require('react');
 
@@ -134,6 +116,10 @@ var _react2 = _interopRequireDefault(_react);
 var _actions = require('./actions');
 
 var actions = _interopRequireWildcard(_actions);
+
+var _reducer = require('./reducer');
+
+var _reducer2 = _interopRequireDefault(_reducer);
 
 var _reactNotificationSystem = require('react-notification-system');
 
@@ -150,6 +136,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+console.log(_reducer2['default']);
 
 var Notifications = function (_React$Component) {
   _inherits(Notifications, _React$Component);
@@ -246,10 +234,8 @@ Object.keys(actions).forEach(function (key) {
   Notifications[key] = actions[key];
 });
 
-// Notifications.reducer = reducer;
+Notifications.reducer = _reducer2['default'];
 
-// module.exports = Notifications;
-// export reducer
-exports['default'] = Notifications;
+module.exports = Notifications;
 
 },{"./actions":1,"./reducer":3,"react":undefined,"react-notification-system":undefined}]},{},[]);
